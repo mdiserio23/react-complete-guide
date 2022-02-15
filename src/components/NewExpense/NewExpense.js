3f04abdac0a8f9dd3aeead6175b868517b538b99
mdiserio <maurizio.diserio@gmail.com>
2022-02-15 14:30:09 +0100

    show form after submit button

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,17 +1,31 @@
+import { useState } from "react";
 import ExpenseForm from "./ExpenseForm";
 import "./NewExpense.css";
 
 const NewExpense = (props) => {
-    const onSaveExpenseHandler = (expenseDataAdded) => {
-        const expense = {
-            ...expenseDataAdded,
-            id: Math.random().toString()
-        }
-        props.addExpense(expense);
-    }
+  const [isEditing, setIsEditing] = useState(false);
+  const onSaveExpenseHandler = (expenseDataAdded) => {
+    const expense = {
+      ...expenseDataAdded,
+      id: Math.random().toString(),
+    };
+    props.addExpense(expense);
+
+    setIsEditing(false);
+  };
+
+  const isEditingHandler = () => {
+    setIsEditing(true);
+  };
+
+  const stopEditingHandler = () => {
+    setIsEditing(false);
+  }
+
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpense={onSaveExpenseHandler}/>
+      {isEditing && <ExpenseForm onSaveExpense={onSaveExpenseHandler} onCancel={stopEditingHandler}/>}
+      {!isEditing && <button onClick={isEditingHandler}>Add Expense</button>}
     </div>
   );
 };
